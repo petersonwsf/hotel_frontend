1baafab0d515d99e10bfc15ca8cf3afb7fb7e88b
Peterson William <petersondev.profissional@gmail.com>
2026-09-01 19:35:05 -0300

    feat: implementando check in e check out em reservas

--- app/(user)/user/reservations/components/ReservationsList.tsx
+++ app/(user)/user/reservations/components/ReservationsList.tsx
@@ -1,20 +1,21 @@
 "use client"
-import { Filter, Reservation } from "@/types/Reservation.types"
+import { Reservation } from "@/types/Reservation.types"
 import { useState } from "react"
-import ReservationFilter from "./ReservationFilter"
 import ReservationCard from "./ReservationCard";
 import Pagination from "@/components/ui/Pagination";
 import Modal from "@/components/ui/Modal";
 import EditReservation from "@/components/reservation/EditReservation/UpdateRerservation";
 import useReservation from "@/hooks/useReservation";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { User } from "@/contexts/AuthContext";
 
-interface UserReservationsProps {
+interface ReservationsListProps {
     reservations: Reservation[];
     pagination : {page : number, totalPages: number}
+    user: User | null;
 }
 
-export default function ReservationsList({ reservations, pagination } : UserReservationsProps) {
+export default function ReservationsList({ reservations, pagination, user } : ReservationsListProps) {
 
     const [selectedReservationId, setSelectedReservationId] = useState<number | undefined>(undefined)
     const [openModalEdit, setOpenModalEdit] = useState<boolean>(false)
@@ -55,7 +56,7 @@ export default function ReservationsList({ reservations, pagination } : UserRese
         <div>
             <div className="flex flex-col gap-[2rem] my-[1.5rem]">
                 {reservations.length > 0  ? reservations.map(reservation => (
-                    <ReservationCard reservation={reservation} key={reservation.id} openEditModal={handleOpenEdit} openDeleteModal={handleOpenDelete} />
+                    <ReservationCard reservation={reservation} key={reservation.id} openEditModal={handleOpenEdit} openDeleteModal={handleOpenDelete} user={user} />
                 )) : (
                     <div className="w-full flex justify-center items-center h-[150px]">
                         <p className="font-light text-gray-500 text-xl">Não há reservas</p>
